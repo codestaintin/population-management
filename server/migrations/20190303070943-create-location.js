d6f030cb92b630a4f55ec3e7609a285622ad8308
Isioye Mohammed <mohammed.isioye@gmail.com>
2019-03-03 09:57:40 +0100

    Setup database - create citizen model - create location model

---
+++ server/migrations/20190303070943-create-location.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      malePopulation: {
+        type: Sequelize.INTEGER
+      },
+      femalePopulation: {
+        type: Sequelize.INTEGER
+      },
+      locality: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Locations');
+  }
+};
---
+++ server/models/location.js
@@ -0,0 +1,33 @@
+export default (sequelize, DataTypes) => {
+  const Location = sequelize.define('Location', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    malePopulation: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    femalePopulation: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    locality: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  }, {});
+  Location.associate = function(models) {
+    // associations can be defined here
+    Location.hasMany(models.Citizen, {
+      foreignKey: 'locationId'
+    })
+  };
+  Location.createRules = () => ({
+    name: 'required|min:2',
+    malePopulation: ['required', 'min:1', 'regex:/^([+-]?[1-9]\\d*|0)$/'],
+    femalePopulation: ['required', 'min:1', 'regex:/^([+-]?[1-9]\\d*|0)$/'],
+    locality: 'required|min:2',
+  });
+  return Location;
+};
